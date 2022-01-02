cd0982b35e68543bb23b37a36100f0d6829f8f4a
Mustafa Elsayed <mskyer@protonmail.com>
2022-01-03 02:59:54 +0300

    added getUserByEmail, createUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserRepository } from './user.repository';
-
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   async getUserById(userId: string): Promise<User> {
-    return this.userRepository.findOne({ _id: userId });
+    return this.userRepository.findOne({ _id: userId }, {});
+  }
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    return this.userRepository.getUserByEmail({ email });
   }
 
   async getUsers(): Promise<User[]> {
     return this.userRepository.find({});
   }
 
-  async createUser(user: CreateUserDto): Promise<User> {
-    return this.userRepository.create(user);
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const salt = await bcrypt.genSalt();
+      const password = await bcrypt.hash(createUserDto.password, salt);
+      const res = await this.userRepository.create({
+        ...createUserDto,
+        password,
+      });
+      return res;
+    } catch (error) {
+      if (error.code === 11000) {
+        // duplicate key error collection
+        throw new HttpException(
+          `#${error.code} duplicate key error collection ${JSON.stringify(
+            error?.keyValue,
+          )}`,
+          HttpStatus.CONFLICT,
+        );
+      } else {
+        throw new HttpException('UNKNOW ERROR', HttpStatus.BAD_REQUEST);
+      }
+    }
   }
 
   async updateUser(id, updateUserDto: UpdateUserDto): Promise<User> {
